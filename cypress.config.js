190e39909ba40f5486e190758d1b4da0d8f7884b
vegardvasset <vva034@student.uib.no>
2024-02-06 10:49:56 +0100

    Init

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    projectId: "q7yjbq",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
 });
